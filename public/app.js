38c4c8a783b9502bde108ba7d1a04ff5f2e114d3
habibsalimov <totetec.dev@gmail.com>
2025-02-10 16:02:11 +0500

    log in

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 class MessageHandler {
     constructor() {
         // Backend API URL'i
-        this.apiUrl = 'http://localhost:5001/api';
+        this.apiUrl = 'http://wordexpert.uz/api';
         // API istekleri için default headers
         this.headers = {
             'Content-Type': 'application/json',
@@ -59,7 +59,7 @@ class MessageHandler {
 class Auth {
     static async loginUser(email, password) {
         try {
-            const response = await fetch('http://localhost:5001/api/token', {
+            const response = await fetch('http://lwordexpert.uz/api/token', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
@@ -85,7 +85,7 @@ class Auth {
 
     static async refreshToken(refreshToken) {
         try {
-            const response = await fetch('http://localhost:5001/api/token/refresh', {
+            const response = await fetch('http://wordexpert.uz/api/token/refresh', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
@@ -116,7 +116,7 @@ class Auth {
         if (!token) return false;
 
         try {
-            const response = await fetch('http://localhost:5001/api/translate', {
+            const response = await fetch('http://wordexpert.uz/api/translate', {
                 method: 'POST',
                 headers: {
                     'Authorization': `Bearer ${token}`,
